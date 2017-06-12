214dbfd3d25fbbd949dcb8b887ea609549605edd
serdar1980 <skomorohovsa@gmail.com>
2017-06-12 22:30:10 +0300

    delete comment

--- L2.1-primitive/src/main/java/ru/otus/Main.java
+++ L2.1-primitive/src/main/java/ru/otus/Main.java
@@ -13,13 +13,11 @@ class Main {
             throw new Exception(e);
         }
         long beforeUsedMem =  r.totalMemory() - r.freeMemory();
-        //long beforeUsedMem =  r.totalMemory();
         Object[] result = new Object[count];
         for (int n = 0; n < count; n++) {
             result[n] = supplier.get();
         }
         long afterUsedMem = r.totalMemory() - r.freeMemory();
-        //long afterUsedMem = r.totalMemory();
         long actualMemUsed = afterUsedMem - beforeUsedMem;
         StringBuilder sb = new StringBuilder();
         sb.append("Average memory for object: ").append(result[0].getClass().getCanonicalName())
@@ -36,14 +34,14 @@ class Main {
 
 
     public static void main(String[] args) throws Exception {
-        //countMemory(100, Main::new);
-       // countMemory(100, Object::new);
-        //countMemory(100, String::new);
-       // countMemory(100, () -> new String(""));
+        countMemory(100, Main::new);
+        countMemory(100, Object::new);
+        countMemory(100, String::new);
+        countMemory(100, () -> new String(""));
         countMemory(100, () -> new String("Вася"));
-       // countMemory(100, () -> new Long(10L));
-       // countMemory(100, () -> new Integer(10));
-       // countMemory(100, () -> new Double(10));
+        countMemory(100, () -> new Long(10L));
+        countMemory(100, () -> new Integer(10));
+        countMemory(100, () -> new Double(10));
     }
 
 }
