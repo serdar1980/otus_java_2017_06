fcb7ba546100a4d00e36377254fed7e39119a5c5
serdar1980 <skomorohovsa@gmail.com>
2017-06-29 20:30:52 +0300

    fix

--- L2.1-primitive/src/main/java/ru/otus/Main.java
+++ L2.1-primitive/src/main/java/ru/otus/Main.java
@@ -4,45 +4,51 @@ import java.util.function.Supplier;
 
 class Main {
 
-    static void countMemory(int count, Supplier<?> supplier) throws Exception {
-        Runtime r=Runtime.getRuntime();
-        r.gc();
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new Exception(e);
-        }
-        long beforeUsedMem =  r.totalMemory() - r.freeMemory();
-        Object[] result = new Object[count];
-        for (int n = 0; n < count; n++) {
-            result[n] = supplier.get();
-        }
-        long afterUsedMem = r.totalMemory() - r.freeMemory();
-        long actualMemUsed = afterUsedMem - beforeUsedMem;
-        StringBuilder sb = new StringBuilder();
-        sb.append("Average memory for object: ").append(result[0].getClass().getCanonicalName())
-            .append("\r\n").append(supplier.get())
-            .append("\r\n").append("size: ").append(actualMemUsed / count);
-        System.out.println(sb.toString());
-        r.gc();
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new Exception(e);
-        }
+  static void countMemory(int count, Supplier<?> supplier) throws Exception {
+    Runtime r = Runtime.getRuntime();
+    r.gc();
+    try {
+      Thread.sleep(1000L);
+    } catch (InterruptedException e) {
+      throw new Exception(e);
     }
 
+    long actualMemUsedSum=0L;
+    Object[] result = new Object[count];
+    for (int n = 0; n < count; n++) {
+      long beforeUsedMem = r.totalMemory() - r.freeMemory();
+      r.gc();
+      result[n] = supplier.get();
+      long afterUsedMem = r.totalMemory() - r.freeMemory();
+      long actualMemUsed = afterUsedMem - beforeUsedMem;
+      actualMemUsedSum +=actualMemUsed;
 
-    public static void main(String[] args) throws Exception {
-        countMemory(100, Main::new);
-        countMemory(100, Object::new);
-        countMemory(100, String::new);
-        countMemory(100, () -> new String(""));
-        countMemory(100, () -> new String("Вася"));
-        countMemory(100, () -> new Long(10L));
-        countMemory(100, () -> new Integer(10));
-        countMemory(100, () -> new Double(10));
     }
+    StringBuilder sb = new StringBuilder();
+    sb.append("Average memory for object: ").append(result[0].getClass().getCanonicalName())
+        .append("\r\n").append(supplier.get())
+        .append("\r\n").append("size: ").append(actualMemUsedSum / count);
+    System.out.println(sb.toString());
+    r.gc();
+    try {
+      Thread.sleep(1000L);
+    } catch (InterruptedException e) {
+      throw new Exception(e);
+    }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    int count = 1999;
+    countMemory(count, Main::new);
+    countMemory(count, Object::new);
+    countMemory(count, String::new);
+    countMemory(count, () -> new String(""));
+    countMemory(count, () -> new String("Вася"));
+    countMemory(count, () -> new Long(10L));
+    countMemory(count, () -> new Integer(10));
+    countMemory(count, () -> new Double(10));
+  }
 
 }
 
