770169a711f11db338d1bd2780b37ceeb33f0e2b
skomorokhov <skomorokhov@i-teco.ru>
2017-08-21 09:46:49 +0300

    delete if code

--- L8_ParserJson/src/main/java/utils/Types.java
+++ L8_ParserJson/src/main/java/utils/Types.java
@@ -1,6 +1,9 @@
 package utils;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public enum Types {
     BYTE,
@@ -17,8 +20,14 @@ public enum Types {
 
     private static final String ALL_TYPES_STRING = Arrays.toString(Types.values());
 
+    private static final Map<Object, Types> map = Arrays.stream(Types.values())
+            .collect(Collectors.toMap(
+                    e -> e.name(), // Это ключ в мапе, имя класса
+                    Function.identity()));
+
     public static Types getType(Class<?> clazz) {
         String className = clazz.getSimpleName().toUpperCase();
+        map.get(clazz.getSimpleName());
         if (ALL_TYPES_STRING.contains(className)) {
             return Types.valueOf(className);
         } else {
