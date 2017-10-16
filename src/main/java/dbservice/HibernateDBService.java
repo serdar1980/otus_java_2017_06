00b419d4c3c8a8025edbca76acd0007d75464481
serdar1980 <skomorohov@gmail.com>
2017-10-16 13:44:20 +0300

    Add web server

--- src/main/java/dbservice/HibernateDBService.java
+++ src/main/java/dbservice/HibernateDBService.java
@@ -11,6 +11,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -54,14 +55,16 @@ public class HibernateDBService implements DBService {
     }
 
     public void save(DataSet dataSet) {
-        Session session = sessionFactory.openSession();
-        session.save(dataSet);
+        try (Session session = sessionFactory.openSession()) {
+            session.save(dataSet);
+        }
     }
 
     @Override
     public <T extends DataSet> DataSet read(long id, Class clazz) {
-        Session session = sessionFactory.openSession();
-        return (T) session.load(clazz, id);
+        try (Session session = sessionFactory.openSession()) {
+            return (T) session.load(clazz, id);
+        }
     }
 
     public DataSet readByName(String name, Class clazz) {
@@ -76,12 +79,14 @@ public class HibernateDBService implements DBService {
     }
 
     public List<? extends DataSet> readAll(Class clazz) {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<DataSet> criteria = builder.createQuery(clazz);
             criteria.from(UserDataSet.class);
             return session.createQuery(criteria).list();
         }
+    }
+
     public void shutdown() {
         sessionFactory.close();
     }
--- src/main/java/ru/otus/web/servlet/AdminServlet.java
+++ src/main/java/ru/otus/web/servlet/AdminServlet.java
@@ -1,10 +1,23 @@
 package ru.otus.web.servlet;
 
+<<<<<<< HEAD
+=======
+import cacheengine.CacheEngine;
+import cacheengine.CacheEngineImpl;
+import dbservice.CacheableDBService;
+import dbservice.HibernateDBService;
+import model.AddressDataSet;
+import model.DataSet;
+import model.PhoneDataSet;
+import model.UserDataSet;
+
+>>>>>>> Add web server
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,13 +27,39 @@ public class AdminServlet extends HttpServlet {
     private static final String ADMIN_PAGE_TEMPLATE = "admin.html";
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+=======
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdminServlet extends HttpServlet {
+    private static final int SIZE = 5;
+
+    private static final CacheEngine<Long, DataSet> cacheEngine =
+            new CacheEngineImpl<>(SIZE, 0, 0, false);
+
+    private static final CacheableDBService dbServiceForCache =
+            new CacheableDBService(new HibernateDBService(), cacheEngine);
+    private static final String DEFAULT_USER_NAME = "UNKNOWN";
+    private static final String ADMIN_PAGE_TEMPLATE = "admin.html";
+
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request, DataSet user) {
+>>>>>>> Add web server
         Map<String, Object> pageVariables = new HashMap<>();
         pageVariables.put("method", request.getMethod());
         pageVariables.put("URL", request.getRequestURL().toString());
         pageVariables.put("locale", request.getLocale());
         pageVariables.put("sessionId", request.getSession().getId());
         pageVariables.put("parameters", request.getParameterMap().toString());
+<<<<<<< HEAD
 
+=======
+        pageVariables.put("cacheHit", dbServiceForCache.getHitCount());
+        pageVariables.put("cacheMiss", dbServiceForCache.getMissCount());
+        pageVariables.put("userInfo", (user != null) ? user.toString() : null);
+>>>>>>> Add web server
         //let's get login from session
         String login = (String) request.getSession().getAttribute(LoginServlet.LOGIN_PARAMETER_NAME);
         pageVariables.put("login", login != null ? login : DEFAULT_USER_NAME);
@@ -28,10 +67,53 @@ public class AdminServlet extends HttpServlet {
         return pageVariables;
     }
 
+<<<<<<< HEAD
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
         Map<String, Object> pageVariables = createPageVariablesMap(request);
+=======
+    private static void someWorkDb() {
+        List<PhoneDataSet> testerPhones = new ArrayList<>();
+        testerPhones.add(new PhoneDataSet("89080980"));
+        testerPhones.add(new PhoneDataSet("84324234"));
+        testerPhones.add(new PhoneDataSet("875756756"));
+
+        UserDataSet testerUser = new UserDataSet("tester", 90);
+        AddressDataSet address = new AddressDataSet("Москва Петровско-разумовская 24 кв 35");
+        testerUser.setAddress(address);
+        testerUser.setPhones(testerPhones);
+
+        for (Long i = 2L; i < 10; i++) {
+            testerUser = new UserDataSet("tester" + i, 90 + i.intValue());
+            address = new AddressDataSet("Москва Петровско-разумовская 24 кв 35");
+            testerUser.setAddress(address);
+            testerUser.setPhones(testerPhones);
+
+            dbServiceForCache.save(testerUser);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        someWorkDb();
+        Map<String, Object> pageVariables = createPageVariablesMap(request, null);
+
+        response.getWriter().println(TemplateProcessor.instance().getPage(ADMIN_PAGE_TEMPLATE, pageVariables));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String paramUserId = request.getParameter("userId");
+        Long searchId = Long.valueOf(paramUserId);
+        DataSet user1 = dbServiceForCache.read(searchId, UserDataSet.class);
+        Map<String, Object> pageVariables = createPageVariablesMap(request, user1);
+>>>>>>> Add web server
 
         response.getWriter().println(TemplateProcessor.instance().getPage(ADMIN_PAGE_TEMPLATE, pageVariables));
 
